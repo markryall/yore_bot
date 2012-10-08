82880129749364a40c8a177bc965f62648199ed3
Mark Ryall <mark@ryall.name>
2012-10-08 17:20:52 +1000

    added use of nick to help message

--- lib/yore_bot.rb
+++ lib/yore_bot.rb
@@ -5,11 +5,17 @@ module YoreBot
   def execute *args
     Cinch::Bot.new do
       store = MessageStore.new
+      nick, server, port, *channels = *args
       configure do |c|
-        c.nick = args.shift
-        c.server = args.shift
-        c.port = args.shift.to_i
-        c.channels = args
+        c.nick = nick
+        c.server = server
+        c.port = port.to_i
+        c.channels = channels
+      end
+
+      on(:message, /^#{nick} help$/) do |m|
+        m.reply "usage:  /msg #{nick} last 10 (seconds/mins/hours) - channel messages within time period"
+        m.reply "code:   https://github.com/markryall/yore_bot"
       end
 
       on :private, /^last (\d+) s/ do |m, count|
